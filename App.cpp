fe968ca5e1db46df414f39ffb5f748185c2ca445
Rodrigo Flores <rodrigo.flores.12.07@gmail.com>
2021-01-08 08:53:30 -0300

    Update: Fixing some bugs and adding some code.

--- App.cpp
+++ App.cpp
@@ -2,20 +2,21 @@
 using namespace std;
 
 int main(){
-  int nem, ranking, pdt_lan, pdt_mat, pdt_sci, pdt_his;
-  int per_nem, per_ranking, _per_pdt_lan, per_pdt_mat, per_pdt_sci, per_pdt_his;
+  double nem, ranking, pdt_lan, pdt_mat, pdt_sci, pdt_his;
+  double per_nem, per_ranking, per_pdt_lan, per_pdt_mat, per_pdt_sci, per_pdt_his;
+  double nem_final, ranking_final, pdt_lan_final, pdt_mat_final, pdt_sci_final, pdt_his_final;
   int option, weight;
 
   cout <<"\t\t\tBienvenido(a) aquí podrás calcular tu puntaje ponderado!"<< "\n\n";
   cout <<"Primero lo primero. ¿Qué prueba ocuparás para ponderar tu puntaje?"<<endl;
   cout <<"Elige: "<<endl;
   cout <<"1) Ciencias.\n"<<"2) Historia." <<endl; cin >> option;
-  cout <<"Ahora, necesitamos saber los porcentajes solicitados en tu ponderacion...";
+  cout <<"Ahora, necesitamos saber los porcentajes solicitados en tu ponderacion..."<<endl;
 
   if(option == 1){
-    cout <<"NEM (Notas de Educacion Media): "; cin >> per_nem;
+    cout <<"\nNEM (Notas de Educacion Media): "; cin >> per_nem;
     cout <<"Ranking: "; cin >> per_ranking;
-    cout <<"Prueba de Matematica: "; cin >> _per_pdt_lan;
+    cout <<"Prueba de Matematica: "; cin >> per_pdt_lan;
     cout <<"Prueba de Lenguaje: "; cin >> per_pdt_mat;
     cout <<"Prueba de Ciencias: "; cin >> per_pdt_sci;
 
@@ -32,15 +33,16 @@ int main(){
     cout <<"Prueba de Lenguaje: "; cin >> pdt_mat;
     cout <<"Prueba de Ciencias: "; cin >> pdt_sci;
 
-    nem *= per_nem;
-    ranking *= per_ranking;
-    pdt_lan *= per_pdt_lan;
-    pdt_mat *= per_pdt_mat;
-    per_pdt_sci *= per_pdt_sci;
+    nem_final = nem * per_nem;
+    ranking_final = ranking * per_ranking;
+    pdt_lan_final = pdt_lan * per_pdt_lan;
+    pdt_mat_final = pdt_mat * per_pdt_mat;
+    pdt_sci_final = pdt_sci * per_pdt_sci;
 
     weight = nem + ranking + pdt_lan + pdt_mat + pdt_sci;
 
     cout <<"Tu ponderacion es: " << weight << endl;
+    // REMEMBER TO CHANGE THE VARIABLES TO FLOAT NUMBERS !!!
 
   }
 
